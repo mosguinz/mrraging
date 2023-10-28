5157908d2122aa7310fbf8745bc1d59e33f60609
mosguinz <mos.guinz@gmail.com>
2023-10-28 13:19:44 -0700

    Got the Hume shit working

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
 import asyncio
-
+import threading
+import cv2
 from hume import HumeStreamClient
-from hume.models.config import BurstConfig
-from hume.models.config import ProsodyConfig
+from hume.models.config import FaceConfig
+
+import base64
+import dotenv
+
+CONFIG = dotenv.dotenv_values()
+
+# Hume
+HUME_API_KEY = CONFIG["HUME_API_KEY"]
+HUME_FPS = 1 / 3  # 3 fps
+hume_config = [FaceConfig()]
+hume_client = HumeStreamClient(HUME_API_KEY)
+
+# CV webcam
+camera = cv2.VideoCapture(0)
+
+
+async def send_to_hume(rb: bytes):
+    async with hume_client.connect(hume_config) as socket:
+        print("Connected to Hume")
+        res = await socket.send_bytes(rb)
+        print(res)  # process return value
+        return res
+
+
+async def webcam_loop():
+    while True:
+        try:
+            _, frame = camera.read()  # grab the current frame
+            frame = cv2.resize(frame, (640, 480))  # resize the frame
+            cv2.imshow("bruh", frame)
+
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+
+            encoded, buffer = cv2.imencode(".jpg", frame)
+            rb = base64.b64encode(buffer)
+            await send_to_hume(rb)
+            # await asyncio.sleep(HUME_FPS)
+
+        except KeyboardInterrupt:
+            break
+
+    camera.release()
+    cv2.destroyAllWindows()
 
 
 async def main():
-    client = HumeStreamClient("<your-api-key>")
-    configs = [BurstConfig(), ProsodyConfig()]
-    async with client.connect(configs) as socket:
-        result = await socket.send_file("<your-audio-filepath>")
-        print(result)
+    await webcam_loop()
 
 
 asyncio.run(main())
